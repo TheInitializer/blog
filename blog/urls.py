7809171b38356e831669f3183dca0010548eb204
David Teresi <dkteresi@gmail.com>
2017-06-28 14:09:32 -0400

    Media! 🎉

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from blog_app import views
 
 urlpatterns = [
@@ -22,3 +24,7 @@ urlpatterns = [
     url(r'^~admin/', admin.site.urls),
     url(r'^(?P<slug>[a-z\-_]+)/', views.post, name='post'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- blog_app/models.py
+++ blog_app/models.py
@@ -18,3 +18,14 @@ class Comment(models.Model):
 	name = models.CharField(max_length=100)
 	content = models.TextField(max_length=1000)
 	post = models.ForeignKey(Post)
+
+class Image(models.Model):
+	file = models.ImageField(upload_to='images')
+
+	def admin_thumbnail(self):
+		return u'<img src="%s" />' % (self.file.url)
+	admin_thumbnail.short_description = 'Thumbnail'
+	admin_thumbnail.allow_tags = True
+
+	def admin_filename(self):
+		return self.file.name
